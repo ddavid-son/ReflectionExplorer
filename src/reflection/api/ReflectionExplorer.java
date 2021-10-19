6b44014244a904cce5d3b4af92049d1104b38664
david davidson <65779614+ddavid-son@users.noreply.github.com>
2021-10-19 17:11:24 +0300

    some more func implemntation

--- src/reflection/api/ReflectionExplorer.java
+++ src/reflection/api/ReflectionExplorer.java
@@ -1,5 +1,7 @@
 package reflection.api;
 
+import jdk.nashorn.internal.codegen.types.Type;
+
 import java.lang.reflect.Constructor;
 import java.lang.reflect.Field;
 import java.lang.reflect.Method;
@@ -39,7 +41,7 @@ public class ReflectionExplorer implements Investigator {
         Class<?>[] Interfaces = this.suspect.getClass().getInterfaces();
         Set<String> interfacesNames = new HashSet<>();
 
-        for(Class<?> inter : Interfaces){
+        for (Class<?> inter : Interfaces) {
             interfacesNames.add(inter.getName());
         }
 
@@ -51,8 +53,8 @@ public class ReflectionExplorer implements Investigator {
         Field[] fields = this.getClass().getDeclaredFields();
         int finalMembersCounter = 0;
 
-        for(Field f : fields){
-            if(Modifier.FINAL == f.getModifiers()) {
+        for (Field f : fields) {
+            if (Modifier.FINAL == f.getModifiers()) {
                 finalMembersCounter++;
             }
         }
@@ -65,8 +67,8 @@ public class ReflectionExplorer implements Investigator {
         Method[] methods = this.suspect.getClass().getDeclaredMethods();
         int staticMethodsCounter = 0;
 
-        for(Method m : methods){
-            if(m.getModifiers() == Modifier.STATIC){
+        for (Method m : methods) {
+            if (m.getModifiers() == Modifier.STATIC) {
                 staticMethodsCounter++;
             }
         }
@@ -80,7 +82,7 @@ public class ReflectionExplorer implements Investigator {
 
     @Override
     public String getParentClassSimpleName() {
-        if(this.isExtending()) {
+        if (this.isExtending()) {
             return this.suspect.getClass().getSuperclass().getName();
         }
 
@@ -89,7 +91,7 @@ public class ReflectionExplorer implements Investigator {
 
     @Override
     public boolean isParentClassAbstract() {
-        if(!(this.getParentClassSimpleName() == null)){
+        if (!(this.getParentClassSimpleName() == null)) {
             return this.suspect.getClass().getSuperclass().getModifiers() == Modifier.ABSTRACT;
         }
 
@@ -100,10 +102,10 @@ public class ReflectionExplorer implements Investigator {
     public Set<String> getNamesOfAllFieldsIncludingInheritanceChain() {
 
         Set<String> allFields = new HashSet<>();
-        if(this.isExtending()) {
-           ReflectionExplorer superClass = new ReflectionExplorer();
-           superClass.load(this.suspect.getClass().getSuperclass());
-           allFields.addAll(superClass.getNamesOfAllFieldsIncludingInheritanceChain());
+        if (this.isExtending()) {
+            ReflectionExplorer superClass = new ReflectionExplorer();
+            superClass.load(this.suspect.getClass().getSuperclass());
+            allFields.addAll(superClass.getNamesOfAllFieldsIncludingInheritanceChain());
         }
 
         Arrays.stream(this.suspect.getClass().getDeclaredFields()).map(s -> s.getName()).forEach(s -> allFields.add(s));
@@ -112,22 +114,27 @@ public class ReflectionExplorer implements Investigator {
 
     @Override
     public int invokeMethodThatReturnsInt(String methodName, Object... args) {
-         return 0;
+        // no method to find a method only by its name.
+        // can be accomplished by getting all the declared methods
+        // and finding one with the same name
+
+
+        return 0;
     }
 
     @Override
     public Object createInstance(int numberOfArgs, Object... args) {
 
-        // is there a problem in using Constructor instead of Class ?
-        Constructor[] constructors = this.suspect.getClass().getConstructors();
-        for(Constructor constructor : constructors){
-            if(constructor.getParameterCount() == numberOfArgs){
+        // is there a problem in using Constructor instead of Class as types here
+        // ask about <?> and is this the right choice or a lazy one
+        Constructor<?>[] constructors = this.suspect.getClass().getConstructors();
+        for (Constructor<?> constructor : constructors) {
+            if (constructor.getParameterCount() == numberOfArgs) {
                 try {
                     // not supposed to use any exception according to aviad
                     return constructor.newInstance(args);
-                }
-                catch (Exception e){
-                    return  null;
+                } catch (Exception e) {
+                    return null;
                 }
             }
         }
@@ -143,6 +150,18 @@ public class ReflectionExplorer implements Investigator {
 
     @Override
     public String getInheritanceChain(String delimiter) {
-        return null;
+
+        Object superClass = this.suspect.getClass().getSuperclass();
+        // need to check if suspect can be ArrayClass(returns Object instead of null)
+        if (superClass == null) {
+            return "Object" + delimiter;
+        }
+
+        ReflectionExplorer target = new ReflectionExplorer();
+        target.load(superClass);
+
+        return target.getInheritanceChain(delimiter) +
+                this.suspect.getClass().getSimpleName()
+                + delimiter;
     }
 }
